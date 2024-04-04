9d72f92671bf695dfe99ec646167824867b2e908
MinaLouisLeon <31774352+MinaLouisLeon@users.noreply.github.com>
2024-04-04 10:17:48 +0200

    Update not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -8,10 +8,10 @@ export default function NotFound() {
 
                 <p className="text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">Uh-oh!</p>
 
-                <p className="mt-4 text-gray-500">We can't find that page.</p>
+                <p className="mt-4 text-gray-500">We can&apos;t find that page.</p>
 
                 <Link
-                    href="#"
+                    href="/"
                     className="mt-6 inline-block rounded bg-indigo-600 px-5 py-3 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring"
                 >
                     Go Back Home
