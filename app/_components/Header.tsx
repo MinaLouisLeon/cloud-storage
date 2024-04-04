c22ee52486a6e7de833ee74952d0a088957e2b40
MinaLouisLeon <31774352+MinaLouisLeon@users.noreply.github.com>
2024-04-04 11:39:37 +0200

    ...

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -53,7 +53,7 @@ const Header = () => {
                                 </>
                             )}
                         </div>
-                        <MobileMenu className="block md:hidden"/>
+                        <MobileMenu />
                     </div>
                 </div>
             </div>
